99499de34df8dde4c8964b49a93d711c2af839b5
H-Drozdowski <hdrozdowski@rrc.ca>
2026-02-01 23:40:19 -0600

    Feat: Created the Shape class and added the required functionality from the class diagram.

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,49 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+from abc import ABC , abstractmethod
+
+__author__ = "Hudson Drozdowski"
+__version__ = "3.13.7"
+
+class Shape(ABC):
+    """Represents a geometrical shape."""
+
+    def __init__(self, color : str):
+        """Initializes a new instance of the Shape class."""
+        
+
+        if len(color.strip()) == 0:
+            raise ValueError("Color cannot be blank.")
+
+        self._color = color
+
+
+
+
+    @abstractmethod
+    def calculate_area(self) -> float:
+        """Calculates the shapes area.
+        
+        Returns:
+            float: The area of the shape.
+        """
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self) -> float:
+        """Calculates the shapes perimeter.
+        
+        Returns
+            float: The perimeter of the shape.
+        """
+        pass
+
+    def __str__(self):
+        """Returns a "informal" string representation of 
+        the Shape object.
+        
+        Returns:
+            str: The "Informal" string representation of
+            the Shape object."""
+
+        return (f"The shape color is {self._color}.")
