fe21af2ce2ceb992ee7a0aa86d81e5d6c62ffc66
H-Drozdowski <hdrozdowski@rrc.ca>
2026-02-01 22:40:57 -0600

    Initial Commit

---
+++ activity_02_main.py
@@ -0,0 +1,41 @@
+""""A client program written to verify correctness of the activity 
+classes.
+"""
+
+__author__ = "ACE Faculty"
+__version__ = "1.0.0"
+__credits__ = ""
+
+def main():
+    """Test the functionality of the methods encapsulated 
+    in this project.
+    """
+
+    # 1. Declare variable that would store a list of Shape objects.
+    # Initialize the variable to an empty list.
+
+
+    # 2. Code a statement which creates an instance of the Triangle 
+    # class.
+    # Append the Triangle to the list of shapes.
+
+
+    # 3. Code a statement which creates an instance of the Rectangle 
+    # class.
+    # Append the Rectangle to the list of shapes.
+
+
+    # 4. Code 3 additional statements which creates an instance of 
+    # Triangle or Rectangle classes (your choice).
+    # Append these instances to the list of shapes.
+
+
+    # 5. Iterate through the list of shapes.  
+    # On each iteration:
+    # - Print the string representation of the shape.
+    # - Print the area of the shape to 2 decimal places.
+    # - Print the perimeter of the shape to 2 decimal places.
+
+
+if __name__ == "__main__":
+    main()
